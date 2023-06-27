2d346ba8cdf1950af64e54f79699e0ee6547a6ab
Ankita16447 <ankita8021462@gmail.com>
2023-06-27 16:17:29 +0530

    added

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,19 +1,17 @@
 import React from 'react';
 // import {useSelector} from 'react-redux';
 import CartItem from '../components/CartItem';
+import cart from '../assets/cart.jpg';
 
 const Cart = () => {
  
   return (
     <div>
-      <img className = "w-full h-60 object cover" 
-      src = "" alt ="" />
+      <img className = "w-full h-80 object cover" src = {cart} alt ="" />
 
       <div className = "max-w-screen-xl mx-auto py-20 flex">
         <CartItem />
-      </div>
-
-      <div className = "w-1/3 bg-[#fafafa] py-6 px-4">
+        <div className = "w-1/3 bg-[#fafafa] py-6 px-4">
         <div className="flex flex-col gap-6 border-b-[1px] border-b-gray-400 pb-6">
           <h2 className = "text-2xl font-medium">cart totals</h2>
           <p className = 'flex item-center gap-4 text-base'>
@@ -33,7 +31,10 @@ const Cart = () => {
 
         <button className = "text-base bg-black text-white w-full py-3 mt-6 hover:bg-gray-800 duration-100">proceed to checkout</button>
       </div>
+      </div>
+
      
+      
     </div>
   )
 }
